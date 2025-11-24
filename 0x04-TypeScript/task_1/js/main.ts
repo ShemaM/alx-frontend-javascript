518295bb459c4791ae0d308cc27b6e2ecaa08aee
Shema Manasseh <shemamanase992@gmail.com>
2025-11-24 16:19:35 +0300

    task 3: add printTeacher function

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,3 +30,12 @@ const director1: Director = {
 };
 
 console.log(director1);
+
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+  return `${firstName} ${lastName}`;
+};
+console.log(printTeacher('John', 'Doe'));
