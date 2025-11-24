d307b469896c40a9eb3f0cf02fb050d1b44840c6
Shema Manasseh <shemamanase992@gmail.com>
2025-11-24 16:49:00 +0300

    task 3: fix printTeacher function to match ALX requirements

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -34,11 +34,13 @@ const director1: Director = {
 console.log(director1);
 
 // Task 3: printTeacher function
+// Task 3: printTeacher
 interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
+  (teacher: { firstName: string; lastName: string }): string;
 }
-const printTeacher: printTeacherFunction = (firstName: string, lastName: string): string => {
-  return `${firstName.charAt(0)}. ${lastName}`;
+
+const printTeacher: printTeacherFunction = ({ firstName, lastName }) => {
+  return `${firstName[0]}. ${lastName}`;
 };
 
-console.log(printTeacher('John', 'Doe'));
+console.log(printTeacher({ firstName: "John", lastName: "Doe" })); // Output: J. Doe
